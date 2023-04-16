307069235902dc996c700e5017c5363955af8461
lining <2357490894@qq.com>
2023-04-16 18:58:46 +0800

    feat:按需引入

--- components.d.ts
+++ components.d.ts
@@ -9,20 +9,33 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElAvatar: typeof import('element-plus/es')['ElAvatar']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElCarousel: typeof import('element-plus/es')['ElCarousel']
+    ElCarouselItem: typeof import('element-plus/es')['ElCarouselItem']
     ElCol: typeof import('element-plus/es')['ElCol']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElLink: typeof import('element-plus/es')['ElLink']
+    ElPopover: typeof import('element-plus/es')['ElPopover']
     ElRow: typeof import('element-plus/es')['ElRow']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSlider: typeof import('element-plus/es')['ElSlider']
     Footer: typeof import('./src/components/layout/footer/Footer.vue')['default']
     Header: typeof import('./src/components/layout/header/Header.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconPark: typeof import('./src/components/common/iconPark.vue')['default']
+    Layout: typeof import('./src/components/layout/Layout.vue')['default']
     Menu: typeof import('./src/components/layout/menu/Menu.vue')['default']
     MenuList: typeof import('./src/components/layout/menu/menuList.vue')['default']
     PlayerAction: typeof import('./src/components/layout/footer/playerAction.vue')['default']
     PlayerController: typeof import('./src/components/layout/footer/playerController.vue')['default']
     PlayerSlier: typeof import('./src/components/layout/footer/playerSlier.vue')['default']
     PlayerSong: typeof import('./src/components/layout/footer/playerSong.vue')['default']
+    Root: typeof import('./src/components/layout/Root.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SearchPop: typeof import('./src/components/layout/header/searchPop.vue')['default']
+    SearchSuggest: typeof import('./src/components/layout/header/searchSuggest.vue')['default']
+    UserInfo: typeof import('./src/components/layout/header/userInfo.vue')['default']
   }
 }
